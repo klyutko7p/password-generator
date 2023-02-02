d16d4070e6018b741dbec5125be619b4f70c73a0
klyutko <klyutko.d@gmail.com>
2023-02-02 17:34:27 +0600

    commit

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import Generator from './pages/Generator.vue';
+import Generator from './pages/Home.vue';
 
 </script>
 
---
+++ src/pages/Home.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <Credits :title="title"></Credits>
+        <Generator></Generator>
+    </div>
+</template>
+
+<script setup lang="ts">
+import Credits from '@/components/Credits.vue';
+import Generator from '@/components/Generator.vue';
+import { ref } from '@vue/runtime-core';
+let title = ref("Генератор паролей")
+
+
+</script>
